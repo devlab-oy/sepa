4828967589165a859d6b00d907e7f99c21d2bc88
mikamyllynen <mika.myllynen@myy.haaga-helia.fi>
2013-04-10 12:29:33 +0300

    fixed spacing hopefully

--- revenge.rb
+++ revenge.rb
@@ -4,26 +4,54 @@
 #classes/methods only to define structure, not necessarily implemented like this
 
 class Transformer
-require 'nokogiri'
+  require 'nokogiri'
 
-	def fill_xml
-	#parses to xml TODO: from objects or attributes, through REST API?
+  def fill_xml
+  #parses to xml TODO: from objects or attributes, through REST API?
   #NOTES:
   #Starts with using a predefined prefilled xml in the first iteration
   #what kinds of messages incoming in the final iteration?
-	end
+  end
 
-	def read_xml
+  def read_xml
   #NOTES:
 	#parses reply from xml to json messages
   #requirements for final implementation?
-	end
+  end
 
   def canonicalization(xml_file)
   	#NOTES:
   	#to canonicalize outgoing xml
   	# xmlns:ec="http://www.w3.org/2001/10/xml-exc-c14n#"
     # targetNamespace="http://www.w3.org/2001/10/xml-exc-c14n#"
+    #  Schema Definition:
+    #
+    #   #<?xml version="1.0" encoding="utf-8"?>
+    #   #<!DOCTYPE schema
+    #   #  PUBLIC "-//W3C//DTD XMLSchema 200102//EN" "http://www.w3.org/2001/XMLSchema.dtd"
+    #   # [
+    #   #   <!ATTLIST schema 
+    #   #     xmlns:ec CDATA #FIXED 'http://www.w3.org/2001/10/xml-exc-c14n#'>
+    #   #   <!ENTITY ec 'http://www.w3.org/2001/10/xml-exc-c14n#'> 
+    #   #   <!ENTITY % p ''>
+    #   #   <!ENTITY % s ''>
+    #   #  ]>
+    #
+    #<schema xmlns="http://www.w3.org/2001/XMLSchema"
+    #       xmlns:ec="http://www.w3.org/2001/10/xml-exc-c14n#"
+    #          targetNamespace="http://www.w3.org/2001/10/xml-exc-c14n#"
+    #          version="0.1" elementFormDefault="qualified">
+    #
+    #    <element name="InclusiveNamespaces"
+    #             type="ec:InclusiveNamespaces"/>
+    #    <complexType name="InclusiveNamespaces">
+    #       <attribute name="PrefixList" type="NMTOKENS"/>
+    #    </complexType>
+    #  </schema>
+    #  DTD:
+    #  <!ELEMENT InclusiveNamespaces    EMPTY >
+    #  <!ATTLIST InclusiveNamespaces
+    #  PrefixList    NMTOKENS    #REQUIRED >
 
     #following for test purposes
     def test_cl4n_node
@@ -36,26 +64,25 @@ require 'nokogiri'
 end 
 
 class Inspector
-require 'nokogiri'
-	@counter = 1
-	def check_xml_against_schema(xml_file, schema_file)
-	#checks validity, required fields, parameters within given constraints etc
+  require 'nokogiri'
+  @counter = 1
+  def check_xml_against_schema(xml_file, schema_file)
+	  #checks validity, required fields, parameters within given constraints etc
 
-		xsd = Nokogiri::XML::Schema(File.read(schema_file))
-  	doc = Nokogiri::XML(File.read(xml_file))
+    xsd = Nokogiri::XML::Schema(File.read(schema_file))
+    doc = Nokogiri::XML(File.read(xml_file))
 
-  	xsd.validate(doc).each do |error|
-    	puts error.message << ", on check #{@counter} - fail"
+    xsd.validate(doc).each do |error|
+      puts error.message << ", on check #{@counter} - fail"
     else
-    	puts "Check #{@counter} - pass"
-    	@counter++
-  	end
-	end
-
+      puts "Check #{@counter} - pass"
+      @counter++
+    end
+  end
 end
 
 class Secretive
-require 'openssl'
+  require 'openssl'
 	#NOTES&TODO:
 	#prepared signature to be used with anything
 	#defined private key (type)
@@ -72,9 +99,10 @@ require 'openssl'
 		#key2.public?
 		#key3 = OpenSSL::PKey::RSA.new File.read 'public_key.pem'
 		#key3.private? # => false
+    #NEEDS base64
 
-		keypair = OpenSSL::PKey::RSA.new(File.read("DEFINE LOCATION.pem"), password)
-		
+    keypair = OpenSSL::PKey::RSA.new(File.read("DEFINE LOCATION.pem"), password)
+
 		#NOTES:
 		#can only be decrypted with public key
 		#private_encrypted = keypair.private_encrypt 'public_document'
@@ -114,22 +142,22 @@ require 'openssl'
     #</wsse:SecurityTokenReference>
     #</ds:KeyInfo>
     #</ds:Signature>
-		cert = OpenSSL::X509::Certificate.new(File.read("DEFINE LOCATION.pem"))
+    cert = OpenSSL::X509::Certificate.new(File.read("DEFINE LOCATION.pem"))
 
-		cert.issuer = name
-		cert.sign key, OpenSSL::Digest::SHA1.new
+    cert.issuer = name
+    cert.sign key, OpenSSL::Digest::SHA1.new
 		#file locations undefined
 		open 'TESTINGFILE.pem', 'w' do |io| io.write cert.to_pem end
-	end
+    end
 
-	def confirm_signature
+    def confirm_signature
 		#TODO: confirm outside sender
     #could also be implemented in decrypt_content   
-	end
+    end
 
-	def compare
+    def compare
 		#not sure if required, can extend encrypt if needed
 		#comparing hash? from incoming or outgoing?
-	end
+    end
 
 end
