7bcbfcb8fe3e8841d466bed2c9d52d45867e09d5
tiere <tommi.tiere@gmail.com>
2013-04-16 16:25:48 +0300

    Trying to use nokogiri to change the signature contents of the file but no luck yet because of namespaces.

--- soap_client.rb
+++ soap_client.rb
@@ -1,5 +1,6 @@
 require 'savon'
 require 'nokogiri'
+require 'openssl'
 
 def get_user_info
     client = Savon.client(wsdl: "BankCorporateFileService_20080616.xml", pretty_print_xml: true)
@@ -8,7 +9,10 @@ def get_user_info
     doc = Nokogiri::XML(f)
     f.close
 
-    response = client.call(:get_user_info, xml: doc.canonicalize.to_s)
+    binary_security_token = doc.xpath('wsse:BinarySecurityToken').first
+    puts binary_security_token
+
+    response = client.call(:get_user_info, xml: doc.to_s)
 end
 
 get_user_info
