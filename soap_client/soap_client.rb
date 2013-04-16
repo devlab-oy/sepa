3a4633123d8bd01c5cd70575c817c0ae70a12883
tiere <tommi.tiere@gmail.com>
2013-04-16 22:05:55 +0300

    Added SSL key

--- soap_client/soap_client.rb
+++ soap_client/soap_client.rb
@@ -2,8 +2,8 @@ require 'savon'
 require 'nokogiri'
 require 'openssl'
 
-def get_user_info
-    client = Savon.client(wsdl: "BankCorporateFileService_20080616.xml", pretty_print_xml: true)
+def send_soap
+    client = Savon.client(wsdl: "BankCorporateFileService_20080616.xml", pretty_print_xml: true, ssl_version: :SSLv2, ssl_cert_file: "ssl_key.pem")
 
     f = File.open("SOAPrequest_GetUserInfo.xml")
     doc = Nokogiri::XML(f)
@@ -12,4 +12,4 @@ def get_user_info
     response = client.call(:get_user_info, xml: doc.to_s)
 end
 
-get_user_info
+send_soap
