205a6b118b975d97565feff5c91c3fd0c5c115fc
tiere <tommi.tiere@gmail.com>
2013-04-18 15:27:30 +0300

    Finished working on application request handling.

--- soap_client/get_user_info.rb
+++ soap_client/get_user_info.rb
@@ -3,6 +3,9 @@ require 'nokogiri'
 require 'openssl'
 require 'base64'
 
+private_key = OpenSSL::PKey::RSA.new File.read 'keys/private.pem'
+cert = OpenSSL::X509::Certificate.new File.read 'keys/cert.pem'
+
 def load_soap_request
   f = File.open("xml_templates/get_user_info_soap_request.xml")
   soap_request = Nokogiri::XML(f)
@@ -33,23 +36,42 @@ def process_application_request
 
   #Canonicalize the application request
   canon_application_request = application_request.canonicalize
-
   return canon_application_request
 end
 
-def sign_application_request(application_request, application_request_signature)
+def sign_application_request(application_request, application_request_signature, private_key, cert)
   #Take digest from application request and set it to the signature
   digest = OpenSSL::Digest.new('sha1', application_request)
   signature_digest = application_request_signature.at_css "DigestValue"
   signature_digest.content = digest
 
-  #Sign the digest with private key
-end
+  #Sign the digest with private key and base64 code it
+  digest_sign = OpenSSL::Digest::SHA1.new
+  signature = private_key.sign(digest_sign, digest.to_s)
+  signature_base64 = Base64.encode64(signature)
 
+  #Add the base64 coded signature to the signature element
+  signature_signature = application_request_signature.at_css "SignatureValue"
+  signature_signature.content = signature_base64
+
+  #Format the certificate and add the it to the certificate element
+  cert_formatted = cert.to_s.split('-----BEGIN CERTIFICATE-----')[1].split('-----END CERTIFICATE-----')[0].gsub(/\s+/, "")
+  signature_certificate = application_request_signature.at_css "X509Certificate"
+  signature_certificate.content = cert_formatted
+
+  #Convert application request to XML and add the signature element to it
+  application_request_xml  = Nokogiri::XML(application_request)
+  software_id = application_request_xml.at_css "SoftwareId"
+  software_id.add_next_sibling(application_request_signature.root)
+
+  #Base64 code the whole application request
+  application_request_base64 = Base64.encode64(application_request_xml)
+
+  return application_request_base64
+end
 def send_soap(soap_request)
   client = Savon.client(wsdl: "wsdl/wsdl_nordea.xml", pretty_print_xml: true, ssl_version: :SSLv2, ssl_cert_file: "keys/ssl_key.pem")
-
   response = client.call(:get_user_info, xml: soap_request.to_xml)
 end
 
-sign_application_request(process_application_request, load_application_request_signature)
+sign_application_request(process_application_request, load_application_request_signature, private_key, cert)
