eb27c69aa689a0e634c53c572394632ac14d94ef
tiere <tommi.tiere@gmail.com>
2013-04-18 16:48:41 +0300

    XML processing now complete for testing the soap client. There seems to be some errors at least for now.

--- soap_client/get_user_info.rb
+++ soap_client/get_user_info.rb
@@ -20,6 +20,13 @@ def load_application_request_signature
   application_request_signature
 end
 
+def load_soap_request_header
+  f = File.open("xml_templates/soap_request_header_template.xml")
+  soap_request_header = Nokogiri::XML(f)
+  f.close
+  soap_request_header
+end
+
 def process_application_request
   #Load the application request from template
   f = File.open("xml_templates/get_user_info_application_request.xml")
@@ -82,13 +89,46 @@ def process_soap_request(soap_request, application_request_base64)
   #Add timestamp
   soap_request_timestamp = soap_request.xpath("//mod:Timestamp", 'mod' => 'http://model.bxd.fi').first
   soap_request_timestamp.content = Time.now
-  puts soap_request
 
+  #Canonicalize the request
+  soap_request.canonicalize
 end
 
-def send_soap(soap_request)
-  client = Savon.client(wsdl: "wsdl/wsdl_nordea.xml", pretty_print_xml: true, ssl_version: :SSLv2, ssl_cert_file: "keys/ssl_key.pem")
-  response = client.call(:get_user_info, xml: soap_request.to_xml)
+def sign_soap_request(soap_request, soap_request_header, private_key, cert)
+  #Take digest from soap request and put it to the signature
+  digest = OpenSSL::Digest.new('sha1', soap_request)
+  signature_digest = soap_request_header.xpath("//ds:DigestValue", 'ds' => 'http://www.w3.org/2000/09/xmldsig#').first
+  signature_digest.content = digest
+  puts soap_request_header
+
+  #Sign the digest with private key and base64 code it
+  digest_sign = OpenSSL::Digest::SHA1.new
+  signature = private_key.sign(digest_sign, digest.to_s)
+  signature_base64 = Base64.encode64(signature)
+
+  #Add the base64 coded signature to the signature element
+  signature_signature = soap_request_header.xpath("//ds:SignatureValue", 'ds' => 'http://www.w3.org/2000/09/xmldsig#').first
+  signature_signature.content = signature_base64
+  puts soap_request_header
+
+  #Format the certificate and add the it to the certificate element
+  cert_formatted = cert.to_s.split('-----BEGIN CERTIFICATE-----')[1].split('-----END CERTIFICATE-----')[0].gsub(/\s+/, "")
+  signature_certificate = soap_request_header.xpath("//wsse:BinarySecurityToken", 'wsse' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd').first
+  signature_certificate.content = cert_formatted
+  puts soap_request_header
+
+  #Merge the body and header of the soap envelope
+  soap_request_xml  = Nokogiri::XML(soap_request)
+  soap_header = soap_request_header.xpath("//soapenv:Header", 'soapenv' => 'http://schemas.xmlsoap.org/soap/envelope/').first
+  soap_header.add_next_sibling(soap_request_xml.xpath("//soapenv:Body", 'soapenv' => 'http://schemas.xmlsoap.org/soap/envelope/').first)
+  soap_request_header
 end
 
-process_soap_request(load_soap_request, sign_application_request(process_application_request, load_application_request_signature, private_key, cert))
+signed_application_request = sign_application_request(process_application_request, load_application_request_signature, private_key, cert)
+
+processed_soap_request = process_soap_request(load_soap_request, signed_application_request)
+
+signed_soap_request = sign_soap_request(processed_soap_request, load_soap_request_header, private_key, cert)
+
+client = Savon.client(wsdl: "wsdl/wsdl_nordea.xml", pretty_print_xml: true, ssl_version: :SSLv2, ssl_cert_file: "keys/ssl_key.pem")
+response = client.call(:get_user_info, xml: signed_soap_request.to_xml)
