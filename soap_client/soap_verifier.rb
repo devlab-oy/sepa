2083e15ac4de3f36eb37b7c3de19d0bb5f4b2fd9
tiere <tommi.tiere@gmail.com>
2013-04-27 12:59:37 +0300

    Some basic funtionality in soap verifier.

--- soap_client/soap_verifier.rb
+++ soap_client/soap_verifier.rb
@@ -1,15 +1,20 @@
 require 'nokogiri'
-require 'openssl'
 require 'base64'
+require 'openssl'
 
-f = File.open("xml_templates/example_responses/soap_response.xml")
-response = Nokogiri::XML(f)
-f.close
+def load(file)
+  f = File.open(file)
+  soap = Nokogiri::XML(f)
+  f.close
+  soap
+end
 
-body = response.xpath("//soapenv:Body", 'soapenv' => 'http://schemas.xmlsoap.org/soap/envelope/')
+def calculate_body_digest(xml_file)
+  body = xml_file.xpath("//soapenv:Body", 'soapenv' => 'http://schemas.xmlsoap.org/soap/envelope/').first
+  bodycanon = body.canonicalize
+  sha1 = OpenSSL::Digest::SHA1.new
+  digestbin = sha1.digest(bodycanon)
+  Base64.encode64(digestbin)
+end
 
-sha1 = OpenSSL::Digest::SHA1.new
-digestbin = sha1.digest(body.to_s)
-digest = Base64.encode64(digestbin)
-
-puts digest
+puts calculate_body_digest(load('xml_templates/example_responses/soap_response.xml'))
