f4884d9e989b7cfc18d578bf4a12d6710b5ee8fc
mikamyllynen <giefsufu@gmail.com>
2013-05-05 12:13:53 +0300

    c & c

--- message_parser/signature.rb
+++ message_parser/signature.rb
@@ -1,7 +1,6 @@
 class Signature
-  #this class is not really needed
+  # This class contains signature attributes, could be used to add
+  # functionality to confirm sender from crendentials
   attr_accessor :digestValue, :signatureValue, :X509Certificate, :X509IssuerName
-  
-  digestValue,signatureValue,X509Certificate,X509IssuerName = ''
 
 end
