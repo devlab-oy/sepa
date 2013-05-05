c01338a6c3817baaa54432cdb37ae77fdde6770e
mikamyllynen <giefsufu@gmail.com>
2013-05-05 12:11:23 +0300

    cleaned up, commented

--- message_parser/userfiletype.rb
+++ message_parser/userfiletype.rb
@@ -2,21 +2,14 @@ class Userfiletype
 
   attr_accessor :targetId, :fileType, :fileTypeName, :country, :direction, :filetypeServices
 
-  #targetId,fileType,fileTypeName,country,direction = ''
-  #filetypeServices = []
-
   #add incoming filetypeservice to array
   def add_filetypeservice(ftservice)
     filetypeServices<<ftservice 
   end
 
-  #to get the full array with possible options
+  # To get parts of array possible options
   def get_filetypeservices
-    #ftservices = Array.new
-    #filetypeServices.each do |ftservice|
-    #  ftservices<<ftservice
-    #end
+    # Add condition checking
     filetypeServices
   end
-
 end
