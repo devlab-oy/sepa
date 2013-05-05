2aea68d71fdc17c28694233aad19f7015a171118
mikamyllynen <giefsufu@gmail.com>
2013-05-05 12:15:39 +0300

    commented

--- message_parser/filedescriptor.rb
+++ message_parser/filedescriptor.rb
@@ -1,7 +1,5 @@
 class Filedescriptor
-
+# Internal class for containment
   attr_accessor :fileReference, :targetId, :serviceId, :serviceIdOwnerName, :fileType, :fileTimestamp, :status
-
-  #fileReference,targetId,serviceId,serviceIdOwnerName,fileType,fileTimestamp,status = ''
 
 end
--- message_parser/filetypeservice.rb
+++ message_parser/filetypeservice.rb
@@ -1,7 +1,5 @@
 class Filetypeservice
-
+# Internal class for containment
   attr_accessor :serviceId, :serviceIdOwnerName, :serviceIdType, :serviceIdText
-
-  #serviceId,serviceIdOwnerName,serviceIdType,serviceIdText = ''
 
 end
