0d948c6f3f45fc1fe867998d2f62e521fa19648e
mikamyllynen <giefsufu@gmail.com>
2013-05-12 13:17:39 +0300

    some test outputs

--- lib/sepa_client_testing_mika.rb
+++ lib/sepa_client_testing_mika.rb
@@ -5,32 +5,32 @@ require 'sepa'
 # payload = File.open("SOLOLMP.DAT").read
 payload = "kissa"
 
-params = {
-  private_key: 'sepa/nordea_testing/keys/nordea.key', # Path for your own private key
-  cert: 'sepa/nordea_testing/keys/nordea.crt',        # Path to your certificate
-  command: :get_user_info,                            # Command :download_file_list, :upload_file, :download_file or :get_user_info
-  customer_id: '11111111',                            # Unique customer ID
-  environment: 'PRODUCTION',                          # Set the environment to be either PRODUCTION or TEST
-  status: 'NEW',                                      # For filtering stuff. Must be either NEW, DOWNLOADED or ALL
-  target_id: '11111111A1',                            # Some specification of the folder which to access in the bank. I have no idea how this works however.
-  file_type: 'TITO',                                  # File types to upload or download:
-  language: 'FI',                                     # Language must be either FI, EN or SV
-  # - LMP300 = Laskujen maksupalvelu (lähtevä)
-  # - LUM2 = Valuuttamaksut (lähtevä)
-  # - KTL = Saapuvat viitemaksut (saapuva)
-  # - TITO = Konekielinen tiliote (saapuva)
-  # - NDCORPAYS = Yrityksen maksut XML (lähtevä)
-  # - NDCAMT53L = Konekielinen XML-tiliote (saapuva)
-  # - NDCAMT54L = Saapuvat XML viitemaksu (saapuva)
-  wsdl: 'sepa/wsdl/wsdl_nordea.xml',                  # The WSDL file used by nordea. Is identical between banks except for the address.
-  content: payload,                                   # The actual payload to send.
-  file_reference: "11111111A12006030329501800000014"  # File reference for :download_file command
-}
+ params = {
+   private_key: 'sepa/nordea_testing/keys/nordea.key', # Path for your own private key
+   cert: 'sepa/nordea_testing/keys/nordea.crt',        # Path to your certificate
+   command: :get_user_info,                            # Command :download_file_list, :upload_file, :download_file or :get_user_info
+   customer_id: '11111111',                            # Unique customer ID
+   environment: 'PRODUCTION',                          # Set the environment to be either PRODUCTION or TEST
+   status: 'NEW',                                      # For filtering stuff. Must be either NEW, DOWNLOADED or ALL
+   target_id: '11111111A1',                            # Some specification of the folder which to access in the bank. I have no idea how this works however.
+   file_type: 'TITO',                                  # File types to upload or download:
+   language: 'FI',                                     # Language must be either FI, EN or SV
+   # - LMP300 = Laskujen maksupalvelu (lähtevä)
+   # - LUM2 = Valuuttamaksut (lähtevä)
+   # - KTL = Saapuvat viitemaksut (saapuva)
+   # - TITO = Konekielinen tiliote (saapuva)
+   # - NDCORPAYS = Yrityksen maksut XML (lähtevä)
+   # - NDCAMT53L = Konekielinen XML-tiliote (saapuva)
+   # - NDCAMT54L = Saapuvat XML viitemaksu (saapuva)
+   wsdl: 'sepa/wsdl/wsdl_nordea.xml',                  # The WSDL file used by nordea. Is identical between banks except for the address.
+   content: payload,                                   # The actual payload to send.
+   file_reference: "11111111A12006030329501800000014"  # File reference for :download_file command
+ }
 
 sepa_client = Sepa::SepaClient.new(params) # You just create the client with the parameters described above.
 
-ar = sepa_client.get_ar_as_base64
-puts ar
+# ar = sepa_client.get_ar_as_base64
+# puts ar
 # response = sepa_client.send                # And use the send method to send the soap request and pray that you get a proper response.
 
 # # Get response body
@@ -104,11 +104,13 @@ puts ar
 
 
 # Testing functionality of the parser side
-# process = Sepa::Applicationresponse.new
-# Comment in 1 out of 3 to debug reader with different types of responses
-# NOTE: These might not work YET
+process = Sepa::ApplicationResponse.new
+# Comment in 1 animate_response out of 3 to debug reader with different types of responses
 # process.animate_response("sepa/nordea_testing/response/download_filelist_response.xml")
+# #puts "Status = NEW"
+# #puts process.list_new_descriptors
 # process.animate_response("sepa/nordea_testing/response/download_file_response.xml")
 # process.animate_response("sepa/nordea_testing/response/get_user_info_response.xml")
 # To test content attribute passing
-# puts process.get_acctstmt_content("sepa/nordea_testing/response/content_053.xml")
+# puts process.get_account_statement_content("sepa/nordea_testing/response/content_053.xml")
+# puts process.get_debit_credit_notification_content("sepa/nordea_testing/response/content_054.xml")
