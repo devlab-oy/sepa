8aeda63089c38c1760472938ce2d71a2ab119b39
tiere <tommi.tiere@gmail.com>
2013-05-15 16:15:47 +0300

    Method for getting digest values from response.

--- lib/sepa/sender_verifier.rb
+++ lib/sepa/sender_verifier.rb
@@ -3,7 +3,12 @@ require 'nokogiri'
 module Sepa
   class SenderVerifier
     def initialize(soap_response)
-      @digest_value = soap_response.
+      @soap_response = Nokogiri::XML(soap_response)
+    end
+
+    def get_digest_values
+      @soap_response.remove_namespaces!
+      digest_nodes = @soap_response.xpath("//Reference/dsig:DigestValue")
     end
 
     def calculate_digest
