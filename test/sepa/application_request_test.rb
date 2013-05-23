20a217486e6474b3576ef855b3097887d5650aea
tiere <tommi.tiere@gmail.com>
2013-05-23 18:26:58 +0300

    More units tests

--- test/sepa/application_request_test.rb
+++ test/sepa/application_request_test.rb
@@ -22,8 +22,19 @@ class TestApplicationRequest < MiniTest::Unit::TestCase
     }
 
     @ar = Sepa::ApplicationRequest.new(@params)
+    @ar_file = Sepa::ApplicationRequest.new(@params)
+    @params[:command] = :get_user_info
+    @ar_get = Sepa::ApplicationRequest.new(@params)
+    @params[:command] = :download_file_list
+    @ar_list = Sepa::ApplicationRequest.new(@params)
+    @params[:command] = :upload_file
+    @ar_up = Sepa::ApplicationRequest.new(@params)
 
     @doc = Nokogiri::XML(Base64.decode64(@ar.get_as_base64))
+    @doc_file = Nokogiri::XML(Base64.decode64(@ar_file.get_as_base64))
+    @doc_get = Nokogiri::XML(Base64.decode64(@ar_get.get_as_base64))
+    @doc_list = Nokogiri::XML(Base64.decode64(@ar_list.get_as_base64))
+    @doc_up = Nokogiri::XML(Base64.decode64(@ar_up.get_as_base64))
   end
 
   def test_xml_templates_are_unmodified
@@ -58,38 +69,93 @@ class TestApplicationRequest < MiniTest::Unit::TestCase
 
   def test_should_have_timestamp_set
     timestamp = Time.strptime(@doc.at_css("Timestamp").content, '%Y-%m-%dT%H:%M:%S%z')
-    assert timestamp <= Time.now && timestamp > (Time.now - 60), "Timestamp was not set correctly"
+    assert timestamp <= Time.now && timestamp > (Time.now - 60),
+    "Timestamp was not set correctly"
   end
 
   def test_should_have_command_set_when_get_user_info
-    @params[:command] = :get_user_info
-    ar = Sepa::ApplicationRequest.new(@params)
-    doc = Nokogiri::XML(Base64.decode64(ar.get_as_base64))
 
-    assert_equal doc.at_css("Command").content, "GetUserInfo"
+    assert_equal @doc_get.at_css("Command").content, "GetUserInfo"
   end
 
   def test_should_have_command_set_when_download_file_list
-    @params[:command] = :download_file_list
-    ar = Sepa::ApplicationRequest.new(@params)
-    doc = Nokogiri::XML(Base64.decode64(ar.get_as_base64))
-
-    assert_equal doc.at_css("Command").content, "DownloadFileList"
+    assert_equal @doc_list.at_css("Command").content, "DownloadFileList"
   end
 
   def test_should_have_command_set_when_download_file
-    @params[:command] = :download_file
-    ar = Sepa::ApplicationRequest.new(@params)
-    doc = Nokogiri::XML(Base64.decode64(ar.get_as_base64))
-
-    assert_equal doc.at_css("Command").content, "DownloadFile"
+    assert_equal @doc_file.at_css("Command").content, "DownloadFile"
   end
 
   def test_should_have_command_set_when_upload_file
-    @params[:command] = :upload_file
-    ar = Sepa::ApplicationRequest.new(@params)
-    doc = Nokogiri::XML(Base64.decode64(ar.get_as_base64))
+    assert_equal @doc_up.at_css("Command").content, "UploadFile"
+  end
 
-    assert_equal doc.at_css("Command").content, "UploadFile"
+  def test_should_have_environment_set
+    assert_equal @doc.at_css("Environment").content, @params[:environment]
+  end
+
+  def test_should_have_software_id_set
+    assert_equal @doc.at_css("SoftwareId").content,
+    "Sepa Transfer Library version " + Sepa::VERSION
+  end
+
+  def test_should_have_status_set_when_download_file_list
+    assert_equal @doc_list.at_css("Status").content, @params[:status]
+  end
+
+  def test_should_have_status_set_when_download_file
+    assert_equal @doc_file.at_css("Status").content, @params[:status]
+  end
+
+  def test_should_not_have_status_set_when_get_user_info
+    assert !@doc_get.at_css("Status")
+  end
+
+  def test_should_not_have_status_set_when_upload_file
+    assert !@doc_up.at_css("Status")
+  end
+
+  def test_should_have_target_id_set_when_download_file_list
+    assert_equal @doc_list.at_css("TargetId").content, @params[:target_id]
+  end
+
+  def test_should_have_target_id_set_when_download_file
+    assert_equal @doc_file.at_css("TargetId").content, @params[:target_id]
+  end
+
+  def test_should_not_have_target_id_set_when_get_user_info
+    assert !@doc_get.at_css("TargetId")
+  end
+
+  def test_should_have_file_type_set_when_download_file_list
+    assert_equal @doc_list.at_css("FileType").content, @params[:file_type]
+  end
+
+  def test_should_have_file_type_set_when_download_file
+    assert_equal @doc_file.at_css("FileType").content, @params[:file_type]
+  end
+
+  def test_should_have_file_type_set_when_upload_file
+    assert_equal @doc_up.at_css("FileType").content, @params[:file_type]
+  end
+
+  def test_should_not_have_file_type_set_when_get_user_info
+    assert !@doc_get.at_css("FileType")
+  end
+
+  def test_should_have_file_reference_set_when_download_file
+    assert_equal @doc_file.at_css("FileReference").content, @params[:file_reference]
+  end
+
+  def test_should_not_have_file_ref_when_download_file_list
+    assert !@doc_list.at_css("FileReference")
+  end
+
+  def test_should_not_have_file_ref_when_get_user_info
+    assert !@doc_get.at_css("FileReference")
+  end
+
+  def test_should_not_have_file_ref_when_upload_file
+    assert !@doc_up.at_css("FileReference")
   end
 end
