e423e2f03ea69ab9373d75daec688dadd0046e25
tiere <tommi.tiere@gmail.com>
2013-05-25 11:51:11 +0300

    Removed unnecessary params validation from client

--- lib/sepa/client.rb
+++ lib/sepa/client.rb
@@ -1,7 +1,6 @@
 module Sepa
   class Client
     def initialize(params)
-      check_required_params(params)
       # Initialize savon client with params
       @client = Savon.client(wsdl: params[:wsdl], pretty_print_xml: true, log_level: :warn)
       @soap = SoapRequest.new(params)
@@ -31,27 +30,6 @@ module Sepa
 
     def get_content_as_string
       Base64.decode64(get_content_as_base64)
-    end
-
-    # Check that the parameters that are needed for all the commands are provided
-    def check_required_params(params)
-      if params[:private_key].nil?
-        raise ArgumentError, "You didn't provide a private key in the params hash."
-      elsif params[:cert].nil?
-        raise ArgumentError, "You didn't provide a certificate in the params hash."
-      elsif !([:get_user_info, :download_file_list, :download_file, :upload_file]
-        .include?(params[:command]))
-        raise ArgumentError, %(Your didn't provide a proper command.
-        Accepted values are :get_user_info, download_file_list, download_file or
-        :upload_file.)
-      elsif params[:customer_id].nil?
-        raise ArgumentError, "You didn't provide a customer id."
-      elsif !(["PRODUCTION", "TEST"].include?(params[:environment]))
-        raise ArgumentError, %(You didn't provide a proper environment.
-          Accepted environments are PRODUCTION or TEST.)
-      elsif params[:wsdl].nil?
-        raise ArgumentError, "You didn't provide a WSDL file in the params hash."
-      end
     end
   end
 end
