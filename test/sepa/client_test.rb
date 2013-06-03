a9b6f7ef8104ef0eedef70e715923e6a911f60a3
tiere <tommi.tiere@gmail.com>
2013-06-03 18:22:12 +0300

    Tests for params validation and testing that savon mocking through observers is working to make sure that no request actually leaves the computer.

--- test/sepa/client_test.rb
+++ test/sepa/client_test.rb
@@ -2,6 +2,9 @@ require File.expand_path('../../test_helper.rb', __FILE__)
 
 class ClientTest < MiniTest::Test
   def setup
+    @wsdl_path = File.expand_path('../../../lib/sepa/wsdl/wsdl_nordea.xml',
+                                  __FILE__)
+
     keys_path = File.expand_path('../nordea_test_keys', __FILE__)
 
     private_key = OpenSSL::PKey::RSA.new File.read "#{keys_path}/nordea.key"
@@ -17,10 +20,20 @@ class ClientTest < MiniTest::Test
       target_id: '11111111A1',
       language: 'FI',
       file_type: 'TITO',
-      wsdl: 'sepa/wsdl/wsdl_nordea.xml',
+      wsdl: @wsdl_path,
       content: Base64.encode64("Kurppa"),
       file_reference: "11111111A12006030329501800000014"
     }
+
+    observer = Class.new {
+
+      def notify(*)
+        HTTPI::Response.new(200, { "Haisuli" => "Haiseva" }, "Vesinokkaelain")
+      end
+
+    }.new
+
+    Savon.observers << observer
   end
 
   def test_should_initialize_with_proper_params
@@ -37,5 +50,51 @@ class ClientTest < MiniTest::Test
     @params.delete(:command)
 
     assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_private_key_missing
+    @params.delete(:private_key)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_cert_missing
+    @params.delete(:cert)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_customer_id_missing
+    @params.delete(:customer_id)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_environment_missing
+    @params.delete(:environment)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_target_id_missing
+    @params.delete(:target_id)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  def test_should_raise_error_if_language_missing
+    @params.delete(:language)
+
+    assert_raises(KeyError) { Sepa::Client.new(@params) }
+  end
+
+  # Temporary test to make sure that savon mocking is working
+  def test_savon_mocking_works
+    client = Sepa::Client.new(@params)
+    response = client.send
+
+    assert_equal response.http.code, 200
+    assert_equal response.http.headers, 'Haisuli' => 'Haiseva'
+    assert_equal response.http.body, 'Vesinokkaelain'
   end
 end
