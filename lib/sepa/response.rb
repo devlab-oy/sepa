28dec54a0d434457f8b805871ea5bb485a173ce7
tiere <tommi.tiere@gmail.com>
2013-06-09 19:11:57 +0300

    Method for finding all digest values in the document.

--- lib/sepa/response.rb
+++ lib/sepa/response.rb
@@ -2,6 +2,30 @@ module Sepa
   class Response
     def initialize(response)
       @response = response
+      @response.remove_namespaces!
     end
+
+    def verify_soap_digests
+      find_digest_values
+    end
+
+    private
+
+      # Finds all reference nodes with digest values in the document and returns
+      # a hash with uri as the key and digest as the value.
+      def find_digest_values
+        references = {}
+        reference_nodes = @response.css('Reference')
+
+        reference_nodes.each do |node|
+          if node.at_css('DigestValue')
+            uri = node.attr('URI')
+            digest_value = node.at_css('DigestValue').content
+            references[uri] = digest_value
+          end
+        end
+
+        references
+      end
   end
 end
--- test/sepa/response_test.rb
+++ test/sepa/response_test.rb
@@ -2,13 +2,19 @@ require File.expand_path('../../test_helper.rb', __FILE__)
 
 class ResponseTest < MiniTest::Test
   def setup
-    @response = Nokogiri::XML(
+    @response_xml = Nokogiri::XML(
       File.read(File.expand_path('../test_files/response.xml',
                                  __FILE__))
     )
+
+    @response = Sepa::Response.new(@response_xml)
   end
 
   def test_should_initialize_with_proper_params
-    assert Sepa::Response.new(@response)
+    assert Sepa::Response.new(@response_xml)
+  end
+
+  def test_should_verify_soap_digests
+    assert_equal @response.verify_soap_digests, true
   end
 end
