2cb5039791432ce81b45bf1c6748cc19409f43f3
tiere <tommi.tiere@gmail.com>
2013-06-09 20:16:22 +0300

    Method for finding all the nodes to verify.

--- lib/sepa/response.rb
+++ lib/sepa/response.rb
@@ -2,30 +2,64 @@ module Sepa
   class Response
     def initialize(response)
       @response = response
-      @response.remove_namespaces!
     end
 
     def verify_soap_digests
-      find_digest_values
+      references = find_digest_values(@response)
+      find_nodes_to_verify(@response, references)
     end
 
     private
 
       # Finds all reference nodes with digest values in the document and returns
       # a hash with uri as the key and digest as the value.
-      def find_digest_values
+      def find_digest_values(doc)
         references = {}
-        reference_nodes = @response.css('Reference')
+        reference_nodes = @response.css(
+          'xmlns|Reference',
+          'xmlns' => 'http://www.w3.org/2000/09/xmldsig#'
+        )
 
         reference_nodes.each do |node|
-          if node.at_css('DigestValue')
-            uri = node.attr('URI')
-            digest_value = node.at_css('DigestValue').content
-            references[uri] = digest_value
-          end
+          uri = node.attr('URI')
+          digest_value = node.at_css(
+            'xmlns|DigestValue',
+            'xmlns' => 'http://www.w3.org/2000/09/xmldsig#'
+          ).content
+
+          references[uri] = digest_value
         end
 
         references
+      end
+
+      def find_nodes_to_verify(doc, references)
+        nodes = {}
+        references.each do |uri, digest_value|
+          uri = uri.sub(/^#/, '')
+          node = doc.at_css(
+            "[wsu|Id='" + uri + "']",
+            'wsu' => 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss' \
+            '-wssecurity-utility-1.0.xsd'
+          )
+
+          nodes[uri] = node
+        end
+
+        nodes
+      end
+
+      def calculate_digest(doc, node)
+        sha1 = OpenSSL::Digest::SHA1.new
+
+        node = doc.at_css(node)
+
+        canon_node = node.canonicalize(
+          mode=Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0,
+          inclusive_namespaces=nil,with_comments=false
+        )
+
+        Base64.encode64(sha1.digest(canon_node)).gsub(/\s+/, "")
       end
   end
 end
