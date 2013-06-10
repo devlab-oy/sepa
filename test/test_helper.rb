739aa219327a72a1f642b9ccff45704a5bbfe7dd
Mika Myllynen <giefsufu@gmail.com>
2013-06-10 12:31:26 +0300

    Added filters to simplecov to ignore vendor and test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/test/"
+  add_filter "/vendor/"
+end
 require 'minitest/autorun'
 require File.expand_path('../../lib/sepa.rb', __FILE__)
