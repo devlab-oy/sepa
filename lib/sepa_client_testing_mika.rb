0fa378cf602d786173890e63cf8de5993d149b9b
Mika Myllynen <giefsufu@gmail.com>
2013-06-11 08:54:17 +0300

    Cert testing files

--- lib/sepa_client_testing_mika.rb
+++ lib/sepa_client_testing_mika.rb
@@ -1,15 +1,60 @@
-# This app can be used to test the functionality of the sepa client
-
+# First the sepa gem is loaded by requiring it
 require 'sepa'
 
-# Testing functionality of the parser side
-process = Sepa::ApplicationResponse.new
-# Comment in 1 animate_response out of 3 to debug reader with different types of responses
-# process.animate_response("sepa/nordea_testing/response/download_filelist_response.xml")
-# #puts "Status = NEW"
-# #puts process.list_new_descriptors
-# process.animate_response("sepa/nordea_testing/response/download_file_response.xml")
-# process.animate_response("sepa/nordea_testing/response/get_user_info_response.xml")
-# Comment out to test content attribute passing
-# puts process.get_account_statement_content("sepa/nordea_testing/response/content_053.xml")
-puts process.get_debit_credit_notification_content("sepa/nordea_testing/response/content_054.xml")
+# A test payload with no actual data
+payload = "test_payload"
+
+# Keys
+private_key = OpenSSL::PKey::RSA.new(File.read("danske/root_certificate/META-INF/DBH12.RSA"))
+cert = OpenSSL::X509::Certificate.new(File.read("danske/root_certificate/DBGROOT_1111110002.cer"))
+
+# The params hash is populated with the data that is needed for gem to function
+params = {
+  # Path for your own private key
+  private_key: private_key,
+
+  # Path to your certificate
+  cert: cert,
+
+  # Command :download_file_list, :upload_file, :download_file or :get_user_info
+  command: :get_user_info,
+
+  # Unique customer ID
+  customer_id: 'ABC123',
+
+  # Set the environment to be either PRODUCTION or TEST
+  environment: 'TEST',
+
+  # For filtering stuff. Must be either NEW, DOWNLOADED or ALL
+  status: 'NEW',
+
+  # Some specification of the folder which to access in the bank. I have no idea how this works however.
+  target_id: 'Danske Bank',
+
+  # Language must be either FI, EN or SV
+  language: 'EN',
+
+  # File types to upload or download:
+  # - LMP300 = Laskujen maksupalvelu (lähtevä)
+  # - LUM2 = Valuuttamaksut (lähtevä)
+  # - KTL = Saapuvat viitemaksut (saapuva)
+  # - TITO = Konekielinen tiliote (saapuva)
+  # - NDCORPAYS = Yrityksen maksut XML (lähtevä)
+  # - NDCAMT53L = Konekielinen XML-tiliote (saapuva)
+  # - NDCAMT54L = Saapuvat XML viitemaksu (saapuva)
+  file_type: 'TITO',
+
+  # The WSDL file used by nordea. Is identical between banks except for the address.
+  wsdl: 'danske/BankCorporateFileService.xml',
+
+  # The actual payload to send.
+  content: payload,
+
+  # File reference for :download_file command
+  file_reference: "11111111A12006030329501800000014"
+}
+
+# You just create the client with the parameters described above.
+sepa_client = Sepa::Client.new(params)
+
+sepa_client.send
