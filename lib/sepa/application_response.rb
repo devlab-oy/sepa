0911a9d4380ef7737d4910a5e6c2d2791a83aa4f
tiere <tommi.tiere@gmail.com>
2013-06-16 17:38:27 +0300

    Schema validation.

--- lib/sepa/application_response.rb
+++ lib/sepa/application_response.rb
@@ -1,8 +1,29 @@
 module Sepa
   class ApplicationResponse
-    def initialize(application_response)
-      @application_response = application_response
-      puts @application_response
+    def initialize(ar)
+      @ar = ar
+
+      if !@ar.respond_to?(:canonicalize)
+        fail ArgumentError,
+          "The application response you provided is not a valid Nokogiri::XML" \
+          " file."
+      elsif !valid_against_ar_schema?(@ar)
+        fail ArgumentError,
+          "The application response you provided doesn't validate against" \
+          " application response schema."
+      end
     end
+
+    private
+
+      def valid_against_ar_schema?(doc)
+        schemas_path = File.expand_path('../../../lib/sepa/xml_schemas',
+                                        __FILE__)
+
+        Dir.chdir(schemas_path) do
+          xsd = Nokogiri::XML::Schema(IO.read('application_response.xsd'))
+          xsd.valid?(doc)
+        end
+      end
   end
 end
--- test/sepa/application_response_test.rb
+++ test/sepa/application_response_test.rb
@@ -6,9 +6,31 @@ class ApplicationResponseTest < MiniTest::Test
 
     @dfl = Nokogiri::XML(File.read("#{responses_path}/dfl.xml"))
     @dfl = Sepa::Response.new(@dfl).application_response
+
+    @uf = Nokogiri::XML(File.read("#{responses_path}/uf.xml"))
+    @uf = Sepa::Response.new(@uf).application_response
+
+    @df = Nokogiri::XML(File.read("#{responses_path}/df.xml"))
+    @df = Sepa::Response.new(@df).application_response
+
+    @gui = Nokogiri::XML(File.read("#{responses_path}/gui.xml"))
+    @gui = Sepa::Response.new(@gui).application_response
   end
 
   def test_should_initialize_with_proper_params
-    Sepa::ApplicationResponse.new(@dfl)
+    assert Sepa::ApplicationResponse.new(@dfl)
+    assert Sepa::ApplicationResponse.new(@uf)
+    assert Sepa::ApplicationResponse.new(@df)
+    assert Sepa::ApplicationResponse.new(@gui)
+  end
+
+  def test_should_complain_if_initialized_with_something_not_nokogiri_xml
+    assert_raises(ArgumentError) { Sepa::ApplicationResponse.new("Jees") }
+  end
+
+  def test_should_complain_if_response_not_valid_against_schema
+    assert_raises(ArgumentError) do
+      Sepa::ApplicationResponse.new(Nokogiri::XML("<ar>text</ar>"))
+    end
   end
 end
