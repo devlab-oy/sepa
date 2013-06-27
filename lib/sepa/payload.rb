59f4dccb359d5cff54c877260ba284d1fca650d3
tiere <tommi.tiere@gmail.com>
2013-06-27 18:33:57 +0300

    Started building the payload xml.

--- lib/sepa/payload.rb
+++ lib/sepa/payload.rb
@@ -2,5 +2,16 @@ module Sepa
   class Payload
     def initialize(params)
     end
+
+    def to_xml
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.Document {
+          xml.send 'pain.001.001.02'
+        }
+      end
+
+      builder.to_xml
+    end
   end
 end
+''
--- test/sepa/payload_test.rb
+++ test/sepa/payload_test.rb
@@ -2,11 +2,20 @@ require File.expand_path('../../test_helper.rb', __FILE__)
 
 class TestPayload < MiniTest::Test
   def setup
+    @schemas_path = File.expand_path('../../../lib/sepa/xml_schemas', __FILE__)
     @params = {}
     @payload = Sepa::Payload.new(@params)
   end
 
   def test_should_initialize_with_hash
     assert Sepa::Payload.new(@params)
+  end
+
+  def test_payload_validates_as_proper_xml
+    xsd = Nokogiri::XML::Schema(File.read("#{@schemas_path}/xml.xsd"))
+    xml = Nokogiri::XML(@payload.to_xml)
+    xsd.validate(xml).each do |error|
+      puts error.message
+    end
   end
 end
