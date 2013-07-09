44b7c39cc43772526a59a28dab71ff269a8847b0
Mika Myllynen <giefsufu@gmail.com>
2013-07-09 09:27:47 +0300

    Started the refactoring process

--- lib/sepa_client_testing_mika.rb
+++ lib/sepa_client_testing_mika.rb
@@ -1,13 +1,6 @@
 # First the sepa gem is loaded by requiring it
 require 'sepa'
 
-# Create 1024bit sha1 private key and generate Certificate Signing Request with it using parameters from cert_req.conf
-#%x(openssl req -newkey rsa:1024 -keyout req_key.pem -keyform PEM -out CSR.csr -outform DER -config cert_req.conf -nodes)
-#%x(rm signing_key.pem)
-
-# Test pin for nordea
-#pin = '1234567890'
-
 # The params hash is populated with the data that is needed for gem to function
 params = {
   # Test pin for nordea
@@ -25,9 +18,7 @@ params = {
   # Set the environment to be either PRODUCTION or TEST
   environment: 'TEST',
 
-  csr_path: 'CSR.csr',
-  # HMAC seal
-  #hmac: hmac,
+  csr_path: 'sepa/nordea_testing/keys/CSR.csr',
 
   # Selected service (For testing: service, For real: ISSUER)
   service: 'service'
@@ -39,4 +30,3 @@ sepa_client = Sepa::Client.new(params)
 
 sepa_client.send
 
-# TODO Saving of certificate from response into a local file
