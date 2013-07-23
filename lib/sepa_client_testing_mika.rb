0cee3eb2afd9720ffa5c2937514cc957aba37bf5
Mika Myllynen <giefsufu@gmail.com>
2013-07-23 14:46:12 +0300

    Updated old debugging files to current structure

--- lib/sepa_client_testing_mika.rb
+++ lib/sepa_client_testing_mika.rb
@@ -1,13 +1,6 @@
 # First the sepa gem is loaded by requiring it
 require 'sepa'
 
-# Create 1024bit sha1 private key and generate Certificate Signing Request with it using parameters from cert_req.conf
-#%x(openssl req -newkey rsa:1024 -keyout req_key.pem -keyform PEM -out CSR.csr -outform DER -config cert_req.conf -nodes)
-#%x(rm signing_key.pem)
-
-# Test pin for nordea
-#pin = '1234567890'
-
 # The params hash is populated with the data that is needed for gem to function
 params = {
   # Test pin for nordea
