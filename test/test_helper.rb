524d2bafbd6dc99cfc4a022a30817f0eb2d43973
Mika Myllynen <giefsufu@gmail.com>
2013-07-24 16:17:20 +0300

    Simplecov needs to be initiated before requiring project files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,8 @@
-require 'minitest/autorun'
-require File.expand_path('../../lib/sepa.rb', __FILE__)
-require File.expand_path('../sepa/fixtures.rb', __FILE__)
-
 require 'simplecov'
-
+require 'minitest/autorun'
 SimpleCov.start do
   add_filter "/test/"
   add_filter "/vendor/"
 end
+require File.expand_path('../../lib/sepa.rb', __FILE__)
+require File.expand_path('../sepa/fixtures.rb', __FILE__)
