b6027242e4fc175fb714babe18e0cc4eab7199dd
Mika Myllynen <giefsufu@gmail.com>
2013-07-25 17:24:15 +0300

    Test2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,5 @@ SimpleCov.start do
   add_filter "/test/"
   add_filter "/vendor/"
 end
-require File.expand_path('../../lib/sepa.rb', __FILE__)
+require File.expand_path('../../lib/sepafm.rb', __FILE__)
 require File.expand_path('../sepa/fixtures.rb', __FILE__)
