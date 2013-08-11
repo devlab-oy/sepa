8f4cc7351aac634298f978f3f5c10997959e481d
tiere <tommi.tiere@gmail.com>
2013-08-11 11:22:40 +0300

    Removed redundant line from danske get bank certificate test file.

--- lib/danske_get_bank_certificate_test.rb
+++ lib/danske_get_bank_certificate_test.rb
@@ -1,14 +1,14 @@
 require 'sepafm'
 
 params = {
-          bank: :danske,
-          target_id: 'Danske FI',
-          language: 'EN',
-          command: :get_bank_certificate,
-          bank_root_cert_serial: '1111110002',
-          customer_id: '360817',
-          environment: 'TEST',
-          key_generator_type: 'software'
+  bank: :danske,
+  target_id: 'Danske FI',
+  language: 'EN',
+  command: :get_bank_certificate,
+  bank_root_cert_serial: '1111110002',
+  customer_id: '360817',
+  environment: 'TEST',
+  key_generator_type: 'software'
 }
 
 # You just create the client with the parameters described above.
@@ -36,6 +36,3 @@ response = Sepa::Response.new(response)
 #   "Issuer: #{ar.certificate.issuer}\n" \
 #   "First day to use this certificate: #{ar.certificate.not_before}\n" \
 #   "Expires: #{ar.certificate.not_after}"
-
-data = response.get_important_data(:get_bank_certificate)
-puts data
