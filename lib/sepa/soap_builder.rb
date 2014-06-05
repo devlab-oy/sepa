db7eb1bbc1ed988c4fcfa91cac9f2f842f433133
Joni Kanerva <joni@devlab.fi>
2014-06-05 16:11:23 +0300

    cleanup code

--- lib/sepa/soap_builder.rb
+++ lib/sepa/soap_builder.rb
@@ -36,106 +36,102 @@ module Sepa
 
     private
 
-
-    def find_correct_bank_extension
-      case @bank
-      when :danske
-        self.extend(DanskeSoapRequest)
-      when :nordea
-        self.extend(NordeaSoapRequest)
-      end
-    end
-
-    def calculate_digest(doc, node)
-      sha1 = OpenSSL::Digest::SHA1.new
-
-      node = doc.at_css(node)
-
-      canon_node = node.canonicalize(
-        mode=Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0,
-        inclusive_namespaces=nil,with_comments=false
-      )
-
-      Base64.encode64(sha1.digest(canon_node)).gsub(/\s+/, "")
-    end
-
-    def calculate_signature(doc, node)
-      sha1 = OpenSSL::Digest::SHA1.new
-
-      node = doc.at_css(node)
-
-      canon_signed_info_node = node.canonicalize(
-        mode=Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0,inclusive_namespaces=nil,
-        with_comments=false
-      )
-
-      signature = @private_key.sign(sha1, canon_signed_info_node)
-
-      Base64.encode64(signature).gsub(/\s+/, "")
-    end
-
-    def load_header_template
-      header_template = File.open("#{SOAP_TEMPLATE_PATH}/header.xml")
-      Nokogiri::XML(header_template)
-    end
-
-    def load_body_template
-      path = "#{SOAP_TEMPLATE_PATH}/"
-
-      case @command
-      when :download_file_list
-        path << "download_file_list.xml"
-      when :get_user_info
-        path << "get_user_info.xml"
-      when :upload_file
-        path << "upload_file.xml"
-      when :download_file
-        path << "download_file.xml"
-      when :get_certificate
-        path << "get_certificate.xml"
-      when :get_bank_certificate
-        path << "danske_get_bank_certificate.xml"
-      when :create_certificate
-        path << "create_certificate.xml"
-      else
-        fail ArgumentError
+      def find_correct_bank_extension
+        case @bank
+        when :danske
+          self.extend(DanskeSoapRequest)
+        when :nordea
+          self.extend(NordeaSoapRequest)
+        end
       end
 
-      Nokogiri::XML(File.open(path))
-    end
+      def calculate_digest(doc, node)
+        sha1 = OpenSSL::Digest::SHA1.new
+        node = doc.at_css(node)
 
-    def set_node(doc, node, value)
-      doc.at_css(node).content = value
-    end
+        canon_node = node.canonicalize(
+          mode = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0,
+          inclusive_namespaces = nil, with_comments = false
+        )
 
-    def add_body_to_header
-      body = @template.at_css('env|Body')
-      @header_template.root.add_child(body)
-      @header_template
-    end
+        Base64.encode64(sha1.digest(canon_node)).gsub(/\s+/, "")
+      end
 
-    def process_header
-      set_node(@header_template, 'wsu|Created', Time.now.utc.iso8601)
-      set_node(@header_template, 'wsu|Expires', (Time.now.utc + 300).iso8601)
+      def calculate_signature(doc, node)
+        sha1 = OpenSSL::Digest::SHA1.new
+        node = doc.at_css(node)
 
-      timestamp_digest = calculate_digest(@header_template,'wsu|Timestamp')
-      dsig = 'dsig|Reference[URI="#dsfg8sdg87dsf678g6dsg6ds7fg"] dsig|DigestValue'
-      set_node(@header_template, dsig, timestamp_digest)
+        canon_signed_info_node = node.canonicalize(
+          mode = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0,
+          inclusive_namespaces = nil, with_comments = false
+        )
 
-      body_digest = calculate_digest(@template, 'env|Body')
-      dsig = 'dsig|Reference[URI="#sdf6sa7d86f87s6df786sd87f6s8fsda"] dsig|DigestValue'
-      set_node(@header_template, dsig, body_digest)
+        signature = @private_key.sign(sha1, canon_signed_info_node)
+        Base64.encode64(signature).gsub(/\s+/, "")
+      end
 
-      signature = calculate_signature(@header_template, 'dsig|SignedInfo')
-      set_node(@header_template, 'dsig|SignatureValue', signature)
+      def load_header_template
+        header_template = File.open("#{SOAP_TEMPLATE_PATH}/header.xml")
+        Nokogiri::XML(header_template)
+      end
 
-      formatted_cert = format_cert(@cert)
-      set_node(@header_template, 'wsse|BinarySecurityToken', formatted_cert)
-    end
+      def load_body_template
+        path = "#{SOAP_TEMPLATE_PATH}/"
 
-    def request_id
-      SecureRandom.hex(5)
-    end
+        case @command
+        when :download_file_list
+          path << "download_file_list.xml"
+        when :get_user_info
+          path << "get_user_info.xml"
+        when :upload_file
+          path << "upload_file.xml"
+        when :download_file
+          path << "download_file.xml"
+        when :get_certificate
+          path << "get_certificate.xml"
+        when :get_bank_certificate
+          path << "danske_get_bank_certificate.xml"
+        when :create_certificate
+          path << "create_certificate.xml"
+        else
+          fail ArgumentError
+        end
+
+        Nokogiri::XML(File.open(path))
+      end
+
+      def set_node(doc, node, value)
+        doc.at_css(node).content = value
+      end
+
+      def add_body_to_header
+        body = @template.at_css('env|Body')
+        @header_template.root.add_child(body)
+        @header_template
+      end
+
+      def process_header
+        set_node(@header_template, 'wsu|Created', Time.now.utc.iso8601)
+        set_node(@header_template, 'wsu|Expires', (Time.now.utc + 300).iso8601)
+
+        timestamp_digest = calculate_digest(@header_template,'wsu|Timestamp')
+        dsig = 'dsig|Reference[URI="#dsfg8sdg87dsf678g6dsg6ds7fg"] dsig|DigestValue'
+        set_node(@header_template, dsig, timestamp_digest)
+
+        body_digest = calculate_digest(@template, 'env|Body')
+        dsig = 'dsig|Reference[URI="#sdf6sa7d86f87s6df786sd87f6s8fsda"] dsig|DigestValue'
+        set_node(@header_template, dsig, body_digest)
+
+        signature = calculate_signature(@header_template, 'dsig|SignedInfo')
+        set_node(@header_template, 'dsig|SignatureValue', signature)
+
+        formatted_cert = format_cert(@cert)
+        set_node(@header_template, 'wsse|BinarySecurityToken', formatted_cert)
+      end
+
+      def request_id
+        SecureRandom.hex(5)
+      end
 
   end
 end
