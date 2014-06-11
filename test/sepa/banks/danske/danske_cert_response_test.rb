25d1fbc407a4d1b9048e5d08e659d62d72850861
tiere <tommi@devlab.fi>
2014-06-11 13:43:22 +0300

    Tests for danske bank root and encryption certs in response

--- test/sepa/banks/danske/danske_cert_response_test.rb
+++ test/sepa/banks/danske/danske_cert_response_test.rb
@@ -12,4 +12,16 @@ class DanskeCertResponseTest < ActiveSupport::TestCase
     refute_nil bank_signing_cert
     assert_equal bank_signing_cert.to_s, DANSKE_BANK_SIGNING_CERT
   end
+
+  test 'should have corrent bank encryption cert with get bank certificate command' do
+    bank_encryption_cert = get_bank_cert_response.bank_encryption_cert
+    refute_nil bank_encryption_cert
+    assert_equal bank_encryption_cert.to_s, DANSKE_BANK_ENCRYPTION_CERT
+  end
+
+  test 'should have correct bank root cert with get bank certificate command' do
+    bank_root_cert = get_bank_cert_response.bank_root_cert
+    refute_nil bank_root_cert
+    assert_equal bank_root_cert.to_s, DANSKE_BANK_ROOT_CERT
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,6 +27,8 @@ DANSKE_TEST_RESPONSE_PATH = "#{ROOT_PATH}/test/sepa/banks/danske/responses/"
 # Test keys
 DANSKE_TEST_KEYS_PATH = "#{ROOT_PATH}/test/sepa/banks/danske/keys/"
 DANSKE_BANK_SIGNING_CERT = File.read "#{DANSKE_TEST_KEYS_PATH}bank_signing_cert.pem"
+DANSKE_BANK_ENCRYPTION_CERT = File.read "#{DANSKE_TEST_KEYS_PATH}bank_encryption_cert.pem"
+DANSKE_BANK_ROOT_CERT = File.read "#{DANSKE_TEST_KEYS_PATH}bank_root_cert.pem"
 
 
 I18n.enforce_available_locales = true
