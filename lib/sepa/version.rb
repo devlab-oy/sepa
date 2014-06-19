8088d4eff175a27f790ae093ca18710d776969a9
Joni Kanerva <joni@devlab.fi>
2014-06-19 13:32:36 +0300

    bump version

--- lib/sepa/version.rb
+++ lib/sepa/version.rb
@@ -1,3 +1,3 @@
 module Sepa
-  VERSION = "0.0.2"
+  VERSION = "0.1.0"
 end
--- test/sepa/sepa_test.rb
+++ test/sepa/sepa_test.rb
@@ -4,7 +4,7 @@ class TestSepa < ActiveSupport::TestCase
 
   def test_version_must_be_defined
     refute_nil Sepa::VERSION
-    assert_equal "0.0.2", Sepa::VERSION
+    assert_equal "0.1.0", Sepa::VERSION
   end
 
 end
