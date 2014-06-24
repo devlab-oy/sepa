ae24359e9dae368a95dcd4d1817244a4b8c5949b
Joni Kanerva <joni@devlab.fi>
2014-06-24 17:00:11 +0300

    NordeaResponse content --> own_signing_cert

--- lib/sepa/banks/nordea/nordea_response.rb
+++ lib/sepa/banks/nordea/nordea_response.rb
@@ -1,24 +1,19 @@
 module Sepa
   class NordeaResponse < Response
 
-    # Nordea get_certificate has application_response in different place
-    def application_response
-      return super unless command == :get_certificate
-      extract_application_response('http://bxd.fi/CertificateService')
-    end
-
-    # Nordea get_certificate has content in different place
-    def content
-      return super unless command == :get_certificate
-      extract_own_cert
-    end
-
-    def extract_own_cert
+    def own_signing_cert
+      ar = extract_application_response('http://bxd.fi/CertificateService')
       at = 'xmlns|Certificate > xmlns|Certificate'
       xmlns = 'http://filetransfer.nordea.com/xmldata/'
-      node = Nokogiri::XML(application_response).at(at, xmlns: xmlns)
+      node = Nokogiri::XML(ar).at(at, xmlns: xmlns)
 
-      Base64.encode64(OpenSSL::X509::Certificate.new(process_cert_value(node.content)).to_s) if node
+      return unless node
+
+      cert_value = process_cert_value node.content
+      cert = OpenSSL::X509::Certificate.new cert_value
+      cert_plain = cert.to_s
+
+      Base64.encode64 cert_plain
     end
 
   end
