afb6f604faf5fbc4b52b26d2075212a8b170f594
tiere <tommi@devlab.fi>
2014-06-25 13:18:49 +0300

    Add constants for commonly used xml namespaces to sepafm.rb

--- lib/sepafm.rb
+++ lib/sepafm.rb
@@ -25,3 +25,10 @@ SCHEMA_PATH = "#{ROOT_PATH}/lib/sepa/xml_schemas"
 SCHEMA_FILE = "#{ROOT_PATH}/lib/sepa/xml_schemas/wsdl.xml"
 AR_TEMPLATE_PATH = "#{ROOT_PATH}/lib/sepa/xml_templates/application_request"
 SOAP_TEMPLATE_PATH = "#{ROOT_PATH}/lib/sepa/xml_templates/soap"
+
+# XML Namespaces
+DSIG = 'http://www.w3.org/2000/09/xmldsig#'
+OASIS_UTILITY = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
+OASIS_SECEXT = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'
+XML_DATA = 'http://bxd.fi/xmldata/'
+BXD = 'http://model.bxd.fi'
