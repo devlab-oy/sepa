5b1c36a0257f5de9c93168eede4c352e2ef74b3f
tiere <tommi@devlab.fi>
2014-08-01 14:48:12 +0300

    Better check for file type

--- lib/sepa/attribute_checks.rb
+++ lib/sepa/attribute_checks.rb
@@ -65,7 +65,7 @@ module Sepa
     def check_file_type
       return unless [:upload_file, :download_file_list, :download_file].include? command
 
-      unless file_type && file_type.size < 35
+      unless file_type && file_type.respond_to?(:size) && file_type.size < 35
         errors.add(:file_type, FILE_TYPE_ERROR_MESSAGE)
       end
     end
