15784461af299568506139172c3db29768e58def
tiere <tommi@devlab.fi>
2015-10-26 16:01:24 +0200

    Bump version

--- test/sepa/sepa_test.rb
+++ test/sepa/sepa_test.rb
@@ -4,7 +4,7 @@ class TestSepa < ActiveSupport::TestCase
 
   def test_version_must_be_defined
     refute_nil Sepa::VERSION
-    assert_equal "1.0.0", Sepa::VERSION
+    assert_equal "1.0.1", Sepa::VERSION
   end
 
 end
