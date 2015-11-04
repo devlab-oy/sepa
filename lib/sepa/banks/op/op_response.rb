193a64d4ef547b51396164557aa8d9134403d172
tiere <tommi@devlab.fi>
2015-11-04 11:09:09 +0200

    Add OpResponse

---
+++ lib/sepa/banks/op/op_response.rb
@@ -0,0 +1,45 @@
+module Sepa
+  # Handles OP specific response logic. Mainly certificate specific stuff.
+  class OpResponse < Response
+    include Utilities
+
+    # Extracts own signing certificate from the response.
+    #
+    # @return [String] own signing certificate as string it it is found
+    # @return [nil] if the certificate cannot be found
+    def own_signing_certificate
+      application_response = extract_application_response(OP_PKI)
+      at                   = 'xmlns|Certificate > xmlns|Certificate'
+      node                 = Nokogiri::XML(application_response).at(at, xmlns: OP_XML_DATA)
+
+      return unless node
+
+      cert_value = process_cert_value node.content
+      cert       = x509_certificate cert_value
+      cert.to_s
+    end
+
+    # Returns the response code in the response. Overrides {Response#response_code} if {#command} is
+    # `:get_certificate`, because the namespace is different with that command.
+    #
+    # @return [String] response code if it is found
+    # @return [nil] if response code cannot be found
+    # @see Response#response_code
+    def response_code
+      return super unless command == :get_certificate
+
+      node = doc.at('xmlns|ResponseCode', xmlns: OP_PKI)
+      node.content if node
+    end
+
+    # Checks whether the certificate embedded in the response soap has been signed with OP's
+    # root certificate.
+    #
+    # @return [true] if certificate is trusted
+    # @return [false] if certificate fails to verify
+    # @see DanskeResponse#certificate_is_trusted?
+    def certificate_is_trusted?
+      verify_certificate_against_root_certificate(certificate, OP_ROOT_CERTIFICATE)
+    end
+  end
+end
---
+++ test/sepa/banks/op/op_response_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class OpResponseTest < ActiveSupport::TestCase
+  test "fails with invalid params" do
+    a = Sepa::OpResponse.new({ response: "Jees", command: "not" })
+    refute a.valid?
+  end
+
+  test "complains if application response is not valid against schema" do
+    a = Sepa::OpResponse.new({ response: "<ar>text</ar>", command: "notvalid" })
+    refute a.valid?
+  end
+end
