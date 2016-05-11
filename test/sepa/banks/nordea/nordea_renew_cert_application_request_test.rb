a65e069534b274cd488c20b0c057eb304ec8c055
Tommi Järvinen <tommi@devlab.fi>
2016-05-11 11:52:30 +0300

    Test that digest is calculated correctly

--- test/sepa/banks/nordea/nordea_renew_cert_application_request_test.rb
+++ test/sepa/banks/nordea/nordea_renew_cert_application_request_test.rb
@@ -50,4 +50,18 @@ class NordeaRenewCertApplicationRequestTest < ActiveSupport::TestCase
   test "content is set correctly" do
     assert_equal format_cert_request(@params[:signing_csr]), @doc.at_css("Content").content
   end
+
+  test 'digest is calculated correctly' do
+    calculated_digest = @doc.at("xmlns|DigestValue", xmlns: 'http://www.w3.org/2000/09/xmldsig#').content
+
+    # Remove signature for calculating digest
+    @doc.at("xmlns|Signature", xmlns: 'http://www.w3.org/2000/09/xmldsig#').remove
+
+    # Calculate digest
+    sha1          = OpenSSL::Digest::SHA1.new
+    actual_digest = encode(sha1.digest(@doc.canonicalize))
+
+    # And then make sure the two are equal
+    assert_equal actual_digest.strip, calculated_digest.strip
+  end
 end
