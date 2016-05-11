31d986f1b4edab7cf792578d7b61f4fbc2eeb7e5
Tommi Järvinen <tommi@devlab.fi>
2016-05-11 13:38:39 +0300

    Add test suite for Nordea renew certificate soap

---
+++ test/sepa/banks/nordea/nordea_renew_cert_request_soap_builder_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class NordeaRenewCertRequestSoapBuilderTest < ActiveSupport::TestCase
+  setup do
+    @params      = nordea_renew_certificate_params
+
+    # Convert the keys here since the conversion is usually done by the client and these tests
+    # bypass the client
+    @params[:own_signing_certificate] = x509_certificate(@params[:own_signing_certificate])
+    @params[:signing_private_key]     = rsa_key(@params[:signing_private_key])
+
+    soap_builder = Sepa::SoapBuilder.new(@params)
+    @doc         = Nokogiri::XML(soap_builder.to_xml)
+  end
+
+  test "validates against schema" do
+    errors = []
+
+    Dir.chdir(SCHEMA_PATH) do
+      xsd = Nokogiri::XML::Schema(IO.read('soap.xsd'))
+      xsd.validate(@doc).each do |error|
+        errors << error
+      end
+    end
+
+    assert errors.empty?, "The following schema validations failed:\n#{errors.join("\n")}"
+  end
+end
