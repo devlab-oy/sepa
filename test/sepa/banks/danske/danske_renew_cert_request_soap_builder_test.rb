61298fb8ddd788b7c822454c00c542a1870836a8
Tommi Järvinen <tommi@devlab.fi>
2016-07-11 15:42:22 +0300

    Test that signature is calculated correctly

--- test/sepa/banks/danske/danske_renew_cert_request_soap_builder_test.rb
+++ test/sepa/banks/danske/danske_renew_cert_request_soap_builder_test.rb
@@ -25,4 +25,17 @@ class DanskeRenewCertRequestSoapBuilderTest < ActiveSupport::TestCase
 
     assert_valid_against_schema 'oasis-200401-wss-wssecurity-secext-1.0.xsd', ws_node
   end
+
+  test 'signature is calculated correctly' do
+    sha1        = OpenSSL::Digest::SHA1.new
+    private_key = rsa_key(@params.fetch(:signing_private_key))
+
+    added_signature = @doc.at("dsig|SignatureValue", dsig: 'http://www.w3.org/2000/09/xmldsig#').content
+
+    signed_info_node = @doc.at("dsig|SignedInfo", dsig: 'http://www.w3.org/2000/09/xmldsig#')
+    signed_info_node = signed_info_node.canonicalize(Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0)
+    actual_signature = encode(private_key.sign(sha1, signed_info_node)).gsub(/\s+/, "")
+
+    assert_equal actual_signature, added_signature
+  end
 end
