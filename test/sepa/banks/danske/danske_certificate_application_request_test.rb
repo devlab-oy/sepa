0df0528e7c16953e6db7d5fd4106578ba20b3438
Tommi Järvinen <tommi@devlab.fi>
2016-07-11 11:27:57 +0300

    Refactor

--- test/sepa/banks/danske/danske_certificate_application_request_test.rb
+++ test/sepa/banks/danske/danske_certificate_application_request_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class DanskeCertificateApplicationRequestTest < ActiveSupport::TestCase
   setup do
-    @cs_params = danske_create_certificate_params
-    @cs_ar     = Sepa::ApplicationRequest.new(@cs_params)
+    @params = danske_create_certificate_params
+    @ar     = Sepa::ApplicationRequest.new(@params)
   end
 
-  test 'should set environment to customertest when test in parameters' do
-    environment_node = @cs_ar.to_nokogiri.at('tns|Environment')
+  test 'environment is set to customertest when test in parameters' do
+    environment_node = @ar.to_nokogiri.at('tns|Environment')
 
     assert_equal 'customertest', environment_node.content
   end
