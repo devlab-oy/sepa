df5094983e674bbc141994348e6257f14aa62171
Tommi Järvinen <tommi@devlab.fi>
2016-07-12 11:17:33 +0300

    Refactor test client

--- test_client/test_client.rb
+++ test_client/test_client.rb
@@ -4,25 +4,29 @@ module Testing
   require_relative "data/certs"
   require_relative "data/params"
 
-  content_clients = {
-    nordea_download_file:      Sepa::Client.new(NORDEA_DOWNLOAD_FILE_PARAMS),
-    nordea_download_file_list: Sepa::Client.new(NORDEA_DOWNLOAD_FILE_LIST_PARAMS),
-    nordea_get_user_info:      Sepa::Client.new(NORDEA_GET_USER_INFO_PARAMS),
-    nordea_upload_file:        Sepa::Client.new(NORDEA_UPLOAD_FILE_PARAMS),
-    op_download_file:          Sepa::Client.new(OP_DOWNLOAD_FILE_PARAMS),
-    op_download_file_list:     Sepa::Client.new(OP_DOWNLOAD_FILE_LIST_PARAMS),
-    op_upload_file:            Sepa::Client.new(OP_UPLOAD_FILE_PARAMS),
-  }
+  content_clients = [
+    :nordea_download_file,
+    :nordea_download_file_list,
+    :nordea_get_user_info,
+    :nordea_upload_file,
+    :op_download_file,
+    :op_download_file_list,
+    :op_upload_file,
+  ].each_with_object({}) do |i, a|
+    a[i] = Sepa::Client.new("#{i.to_s.upcase}_PARAMS".constantize)
+  end
 
-  certificate_clients = {
-    danske_create_cert:          Sepa::Client.new(DANSKE_CREATE_CERT_PARAMS),
-    danske_get_bank_cert:        Sepa::Client.new(DANSKE_GET_BANK_CERT_PARAMS),
-    danske_renew_cert:           Sepa::Client.new(DANSKE_RENEW_CERT_PARAMS),
-    nordea_get_certificate:      Sepa::Client.new(NORDEA_GET_CERTIFICATE_PARAMS),
-    nordea_renew_certificate:    Sepa::Client.new(NORDEA_RENEW_CERTIFICATE_PARAMS),
-    op_get_certificate:          Sepa::Client.new(OP_GET_CERTIFICATE_PARAMS),
-    op_get_service_certificates: Sepa::Client.new(OP_GET_SERVICE_CERTIFICATES_PARAMS),
-  }
+  certificate_clients = [
+    :danske_create_cert,
+    :danske_get_bank_cert,
+    :danske_renew_cert,
+    :nordea_get_certificate,
+    :nordea_renew_certificate,
+    :op_get_certificate,
+    :op_get_service_certificates,
+  ].each_with_object({}) do |i, a|
+    a[i] = Sepa::Client.new("#{i.to_s.upcase}_PARAMS".constantize)
+  end
 
   content_clients.each do |name, client|
     response = client.send_request
