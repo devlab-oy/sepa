5cb41d1477eea3fbb8b97bc86e75b3567cdebd42
Joni Kanerva <joni@devlab.fi>
2016-07-12 13:19:28 +0300

    bump version

--- lib/sepa/version.rb
+++ lib/sepa/version.rb
@@ -1,4 +1,4 @@
 module Sepa
   # The current version of the gem
-  VERSION = "1.1.5".freeze
+  VERSION = "1.1.6".freeze
 end
--- test/sepa/sepa_test.rb
+++ test/sepa/sepa_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class TestSepa < ActiveSupport::TestCase
   test 'version is defined correctly' do
     refute_nil Sepa::VERSION
-    assert_equal "1.1.5", Sepa::VERSION
+    assert_equal "1.1.6", Sepa::VERSION
   end
 end
