12210778ad9300db7f07783338f69f5817f20776
Tommi Järvinen <tommi.tiere@gmail.com>
2017-09-25 18:40:55 +0100

    Bump version

--- test/sepa/sepa_test.rb
+++ test/sepa/sepa_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class TestSepa < ActiveSupport::TestCase
   test 'version is defined correctly' do
     refute_nil Sepa::VERSION
-    assert_equal '1.1.9', Sepa::VERSION
+    assert_equal '1.1.10', Sepa::VERSION
   end
 end
