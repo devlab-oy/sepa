4ebcaf2db7d1b3e8cf168a11bc2e2cbaa0a2f298
Tommi Järvinen <tommi@ahkio.com>
2022-11-09 12:29:53 +0200

    Update version

--- lib/sepa/version.rb
+++ lib/sepa/version.rb
@@ -2,7 +2,7 @@ module Sepa
   # The current version of the gem
   MAJOR = 1
   MINOR = 1
-  TINY  = 16
+  TINY  = 17
   PRE   = nil
 
   VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
--- test/sepa/sepa_test.rb
+++ test/sepa/sepa_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class TestSepa < ActiveSupport::TestCase
   test 'version is defined correctly' do
     refute_nil Sepa::VERSION
-    assert_equal '1.1.16', Sepa::VERSION
+    assert_equal '1.1.17', Sepa::VERSION
   end
 end
