32834386de5183991f83c6701fd078c8bf4590b9
Olli Björkqvist <olli.bjorkqvist@sprintit.fi>
2025-02-24 15:24:55 +0200

    nordea SLL troubles

--- lib/sepa/client.rb
+++ lib/sepa/client.rb
@@ -224,7 +224,7 @@ module Sepa
       self.language      ||= 'EN'
       self.status        ||= 'NEW'
       self.savon_options ||= {
-        globals: {ssl_version: :TLSv1_2},
+        globals: {ssl_version: :TLSv1_2, ssl_verify_mode: :none},
         locals: {},
       }
     end
